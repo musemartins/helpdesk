d88a755b5e5e0662fc4e38d58a669894304528fb
unknown <carlosmartins353@gmail.com>
2016-03-02 17:59:59 +0000

    Initial migrations

--- database/migrations/2016_03_02_173937_create_comments_table.php
+++ database/migrations/2016_03_02_173937_create_comments_table.php
@@ -14,6 +14,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('comment');
+            $table->integer('project_id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
--- database/migrations/2016_03_02_175103_create_issue_table.php
+++ database/migrations/2016_03_02_175103_create_issue_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFilesTable extends Migration
+class CreateQuestionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,12 @@ class CreateFilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('files', function (Blueprint $table) {
+        Schema::create('issue', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('path');
-            $table->string('user_id');
+            $table->string('assigned_to');
+            $table->text('question');
+            $table->integer('user_id');
+            $table->integer('status');
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ class CreateFilesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('files');
+        Schema::drop('issue');
     }
 }
